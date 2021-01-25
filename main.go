96b9f6367aad0fdc78cf8e20ac6f96c341fc786d
Robbie Scheidt <robert.scheidt@coursehero.com>
2021-01-25 13:33:15 -0800

    init commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	jpeg "image/jpeg"
+	png "image/png"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/disintegration/imaging"
+)
+
+func main() {
+	profileUrl := os.Args[1]
+	fmt.Printf("Start with source image, %s\n", profileUrl)
+
+	// load template
+	templateFile, err := os.Open("./template.png")
+	if err != nil {
+		log.Fatalln("Could not open template", err)
+	}
+	defer templateFile.Close()
+
+	templateImage, err := png.Decode(templateFile)
+	if err != nil {
+		log.Fatalln("Could not decode image file", err)
+	}
+	width := templateImage.Bounds().Max.X
+	height := templateImage.Bounds().Max.Y
+	fmt.Printf("Template Image width: %d, height: %d\n", width, height)
+
+	// download remote image from url
+	response, err := http.Get(profileUrl)
+	if err != nil {
+		log.Fatalln("Could not download remote image", err)
+	}
+
+	defer response.Body.Close()
+	profileImage, err := png.Decode(response.Body)
+	if err != nil {
+		log.Fatalln("Could not decode remote image file", err)
+	}
+	profileImage = imaging.Resize(profileImage, 600, 0, imaging.Lanczos)
+	profileImage = imaging.Rotate(profileImage, 40.0, color.Transparent)
+
+	// how to resize: dstImage2 := imaging.Resize(src2, 256, 256, imaging.Lanczos)
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+	dst := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// draw profile
+	// bnds := image.Rectangle{
+	// 	image.Point{20, 20},
+	// 	image.Point{300, 300},
+	// }
+	draw.Draw(dst, profileImage.Bounds(), profileImage, image.ZP, draw.Src)
+
+	// draw template over it
+	draw.Draw(dst, templateImage.Bounds(), templateImage, image.ZP, draw.Over)
+
+	out, err := os.Create("./output.jpg")
+	if err != nil {
+		log.Fatalln("Could create file", err)
+	}
+
+	var opt jpeg.Options
+	opt.Quality = 80
+	jpeg.Encode(out, dst, &opt)
+
+	fmt.Println("End")
+}
